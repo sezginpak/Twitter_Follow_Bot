16f67fa153eff37950ebe67eadb4d64f358d4203
Sezgin <sezginpak@sezginpak.me>
2019-07-18 18:36:37 +0300

    Created...

---
+++ config.py
@@ -0,0 +1,28 @@
+oauth_configure = {
+    "CONSUMER_API_KEY": "",
+    "CONSUMER_API_SECRET_KEY": "",
+    "ACCESS_TOKEN": "",
+    "ACCESS_TOKEN_SECRET": "",
+}
+
+bot_settings = {
+    "FOLLOW_TIME": 240,
+    "UNFOLLOW_TIME": 240,
+    "FOLLOW_PER_DAY": None,
+    "UNFOLLOW_PER_DAY": None,
+    "USER_MAX_FOLLOW": 500,
+    "USER_MIN_FOLLOW": 250,
+    "USER_DEFAULT_FOLLOW": [
+    "",
+    "",
+    ],
+    "USER_BLACKLIST": [
+    "",
+    "",
+    ],
+    "FOLLOW_RECENT_FEED": True,
+    "END_AT_H": 2,
+    "END_AT_M": 30,
+    "START_AT_H": 9,
+    "START_AT_M": 30,
+}
